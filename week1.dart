5bb3e78dd5b7351af36f1e6312a48609bef57ee7
Abdurrahmaan9 <114228057+Abdurrahmaan9@users.noreply.github.com>
2023-03-08 14:40:54 +0200

    data types
    data types in dart

---
+++ week1.dart
@@ -0,0 +1,25 @@
+void main(List<String> args) {
+  // int is used to declare data of type numbers
+  int num = 10;
+  // double is used to store numbers of type fractions or with decimal points
+  double num2 = 9.99;
+  // string is for type sentences
+  String intro = 'my name is abdul';
+  // list is for data of deferent data types for example it can store ints, doubles, strings and maps 
+  List names = [
+    'asiatu',
+    'lazarous',
+    'mariam',
+    'ayub',
+    'Aadam',
+    'lazarous',
+    'jamilah',
+    'austin',
+    'abdul',
+  ];
+  // Map imo = ;
+  print(num);
+  print(num2);
+  print(intro);
+  print(names);
+}
